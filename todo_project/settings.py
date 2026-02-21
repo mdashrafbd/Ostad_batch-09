35c849f901a70a6483dc6da78f0d98f904bb3923
Ashraf Uddin <mdashrafbd@gmail.com>
2026-02-22 01:10:17 +0600

    Fixed again ALLOWED_HOSTS for Render deployment

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [
     'subclimatic-sherri-nonprovocatively.ngrok-free.dev',
     '.ngrok-free.dev',  # Allow all ngrok-free.dev subdomains
     '.ngrok.io',  # Allow all ngrok.io subdomains (for paid ngrok)
-    'testtttttt-kf9v.onrender.com'
+    'testtttttt-kf9v.onrender.com',
     'todo-ashraf.onrender.com'
 ]
 
